0c35e2313fe6b3cf3891cd09b1d44668b565210b
anmolalgorist7110 <anmolalgo12345@gmail.com>
2023-06-05 13:50:10 +0530

    add new file name gradient_container.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
-      MaterialApp(
+      const MaterialApp(
       home: Scaffold(
        body: GradientContainer(),
       ),
@@ -10,28 +11,3 @@ void main() {
   );
 }
 
-class GradientContainer extends StatelessWidget{
-  @override
-  Widget build (BuildContext context){
-      return   Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                 Color.fromARGB(255, 26, 2, 80) ,
-                  Color.fromARGB(255, 45, 20, 98) ,
-                ],
-                begin: Alignment.topLeft ,
-                end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text('Hello World !' , style: TextStyle(
-             color: Colors.white ,
-             fontSize: 28 ,
-
-            ),
-            ),
-          ),
-        );
-  }
-}
