7be7eb66d3bc70c65b243b5f43be4150b7091895
anmolalgorist7110 <anmolalgo12345@gmail.com>
2023-06-05 13:12:48 +0530

    add stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-     MaterialApp(
+      MaterialApp(
       home: Scaffold(
-        body: Container(
+       body: GradientContainer(),
+      ),
+    ),
+  );
+}
+
+class GradientContainer extends StatelessWidget{
+  @override
+  Widget build (BuildContext context){
+      return   Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
                  Color.fromARGB(255, 26, 2, 80) ,
-                  Color.fromARGB(255, 45, 7, 98) ,
+                  Color.fromARGB(255, 45, 20, 98) ,
                 ],
                 begin: Alignment.topLeft ,
                 end: Alignment.bottomRight,
@@ -19,12 +28,10 @@ void main() {
             child: Text('Hello World !' , style: TextStyle(
              color: Colors.white ,
              fontSize: 28 ,
-             
+
             ),
             ),
           ),
-        ),
-      ),
-    ),
-  );
+        );
+  }
 }
